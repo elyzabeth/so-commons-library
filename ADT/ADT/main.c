4036c2c300e092aaf56888425226c7675cb660e7
JoseMariaSola <josemariasola@Josys-MacBook-Air.local>
2014-09-06 20:37:36 -0300

    .gitignore updated for Xcode file types
    .gitignore updated for Xcode file types

---
+++ ADT/ADT/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ADT
+//
+//  Created by José María Sola on 9/6/14.
+//  Copyright (c) 2014 José María Sola. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
